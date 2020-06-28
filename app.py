7b6bd2774bb37ac77a0e3d75bb212acaf23dee63
Alex Quach <alexhq1000@gmail.com>
2020-06-28 18:10:00 +0000

    commit to web app

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 import pandas as pd
 import numpy as np
 import pickle
+import json
+from sklearn.linear_model import LogisticRegression, LinearRegression
 
 from fairml import audit_model
 #from fairml import plot_generic_dependence_dictionary
@@ -21,7 +23,7 @@ heading = "Eval ML Heading"
 
 client = MongoClient(MONGO_CLIENT)
 db = client['fairrank']
-collection = db['scores']
+collection = db['scores_prod']
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,23 +32,60 @@ def index():
         # User is requesting the form
         return render_template('form.html')
     elif request.method == 'POST':
-        data = pd.read_csv(request.files['datafile'])
-        model_pickle = request.files['modelfile']
-        model = pickle.load(model_pickle)
+        #data = pd.read_csv(request.files['datafile'])
+        #model_pickle = request.files['modelfile']
+        #model = pickle.load(model_pickle)
+
+        ### TEMP 
+    
+        loans_data = pd.read_csv(
+            filepath_or_buffer="test_data/full_data_loans.csv")
+
+        loans_data = loans_data.drop("Loan_ID", 1)
+        cat_list = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', "Property_Area", "Loan_Status"]
+        for name in cat_list:
+            loans_data[name] = loans_data[name].astype('category')
+
+        loans_data = loans_data.dropna()
+
+        cat_columns = loans_data.select_dtypes(['category']).columns
+        loans_data[cat_columns] = loans_data[cat_columns].apply(lambda x: x.cat.codes)
+
+        y = loans_data.Loan_Status.values
+        loans_data = loans_data.drop("Loan_Status", 1)
+
+
+        #loans_data.to_csv('../test_data/loans.csv', index=False)
+
+        model = LogisticRegression()
+        model_name = "LogisticRegression"
+
+        # train model
+        model.fit(loans_data.values, y)
+
+        ### TEMP
+
         #  call audit model with model
-        importances, _ = audit_model(model.predict, data)
+        importances, _ = audit_model(model.predict, loans_data)
+        print('printing importances')
+        print(importances)
 
         importances_dict = {}
         for key, value in dict(importances).items():
             importances_dict[key] = np.median(np.array(value))
 
         data_dict = {'importances': importances_dict}
-        data_dict['dataset'] = request.files['datafile'].name
+        data_dict['dataset'] = request.files['datafile'].filename
+        data_dict['model'] = request.files['modelfile'].filename
         data_dict['public_sharing'] = False
 
         document_id = collection.insert(data_dict)
 
-        return render_template('result.html', document_id=document_id, importances_dict=importances_dict)
+        features_array = list(importances_dict.keys())
+        importances_array = list(importances_dict.values())
+
+        return render_template('result.html', document_id=document_id, features_array=json.dumps(features_array), 
+            importances_array=json.dumps(importances_array))
 
 @app.route('/result', methods=['POST'])
 def results():
@@ -68,6 +107,30 @@ def results():
 @app.route('/ranking', methods=['GET'])
 def ranking():
     if request.method == 'GET':
+
+        collection.find({'dataset'})
+        # 
+        """
+
+        {
+            "datasetname1": {
+                features: []
+                models: {
+                    modelname1: [list of scores]
+                    modelname2: [list of scores]
+                }
+            },
+            "datasetname1": {
+                features: []
+                models: {
+                    modelname1: [list of scores]
+                    modelname2: [list of scores]
+                }
+            }
+
+        }
+        """
+
         return render_template('ranking.html')
 
 if __name__ == "__main__":
--- test_models/train_compas.py
+++ test_models/train_compas.py
@@ -7,14 +7,64 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
 
 # read in the propublica data to be used for our analysis.
-propublica_data = pd.read_csv(
-    filepath_or_buffer="../test_data/propublica_data_for_fairml.csv")
+# propublica_data = pd.read_csv(
+#     filepath_or_buffer="../test_data/propublica_data_for_fairml.csv")
+
+# # create feature and design matrix for model building.
+# compas_rating = propublica_data.score_factor.values
+# propublica_data = propublica_data.drop("score_factor", 1)
+
+# propublica_data.to_csv('../test_data/propublica.csv', index=False)
+
+
+loans_data = pd.read_csv(
+    filepath_or_buffer="../test_data/full_data_loans.csv")
 
 # create feature and design matrix for model building.
-compas_rating = propublica_data.score_factor.values
-propublica_data = propublica_data.drop("score_factor", 1)
 
-propublica_data.to_csv('../test_data/propublica.csv', index=False)
+loans_data = loans_data.drop("Loan_ID", 1)
+cat_list = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', "Property_Area", "Loan_Status"]
+for name in cat_list:
+    loans_data[name] = loans_data[name].astype('category')
+
+loans_data = loans_data.dropna()
+
+cat_columns = loans_data.select_dtypes(['category']).columns
+loans_data[cat_columns] = loans_data[cat_columns].apply(lambda x: x.cat.codes)
+
+y = loans_data.Loan_Status.values
+loans_data = loans_data.drop("Loan_Status", 1)
+
+
+loans_data.to_csv('../test_data/loans.csv', index=False)
+
+model = LogisticRegression()
+model_name = "LogisticRegression"
+
+# train model
+model.fit(loans_data.values, y)
+
+# pickle model
+path = './loans/' + model_name + '.pkl'
+filehandler = open(path, 'wb')
+pickle.dump(model, filehandler)
+filehandler.close()
+
+
+
+# model_names = ['LinearRegression', 'LogisticRegression', 'RandomForest', 'Adaboost', 'DecisionTree']
+# models = [LinearRegression(), LogisticRegression(penalty='l2', C=0.01), RandomForestClassifier(random_state=0), AdaBoostRegressor(), DecisionTreeRegressor()]
+
+# for model_name, model in zip(model_names, models):
+#     # train model
+#     model.fit(loans_data.values, y)
+
+#     # pickle model
+#     path = './loans/' + model_name + '.pkl'
+#     filehandler = open(path, 'wb')
+#     pickle.dump(model, filehandler)
+#     filehandler.close()
+
 
 # this is just for demonstration, any classifier or regressor
 # can be used here. fairml only requires a predict function
@@ -32,18 +82,4 @@ propublica_data.to_csv('../test_data/propublica.csv', index=False)
 
 # print(clf)
 
-
-model_names = ['LinearRegression', 'LogisticRegression', 'RandomForest', 'Adaboost', 'DecisionTree']
-models = [LinearRegression(), LogisticRegression(penalty='l2', C=0.01), RandomForestClassifier(random_state=0), AdaBoostRegressor(), DecisionTreeRegressor()]
-
-for model_name, model in zip(model_names, models):
-
-    # train model
-    model.fit(propublica_data.values, compas_rating)
-
-    # pickle model
-    path = './' + model_name + '.pkl'
-    filehandler = open(path, 'wb')
-    pickle.dump(model, filehandler)
-    filehandler.close()
 
